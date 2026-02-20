3e935e4f62b44c30dbbcf3ad25965248bd6878dd
Pedro Lucas <133167137+pedrocaniato@users.noreply.github.com>
2026-02-20 14:05:11 +0000

    Adicionando o Pages

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home () {
+    return <h1>
+        Borroni, Borroni,
+        teu nome rima com pepperoni,
+        se você não me amar
+        eu vou chorar no telefone 📞😔
+    </h1>
+}
+
+export default Home;
